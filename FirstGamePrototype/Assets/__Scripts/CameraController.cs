de6fd571b697bc7d85fc591e1359e23a25916a3c
Jack Cochran <64762170+jackscochran@users.noreply.github.com>
2021-02-16 19:29:46 -0700

    Added camera movement and laserzzz

---
+++ FirstGamePrototype/Assets/__Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Script that makes camera follow the player
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public float xLowerBound;
+    public float xUpperBound;
+    public float yLowerBound;
+    public float yUpperBound;
+
+    float height;
+    float width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera cam = gameObject.GetComponent<Camera>();
+        height = 2f * cam.orthographicSize;
+        width = height * cam.aspect;
+
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if(player.transform.position.x > xLowerBound + width/2 && player.transform.position.x < xUpperBound - width/2)
+            transform.position = new Vector3(offset.x + player.transform.position.x, transform.position.y, -10);
+
+    }
+
+    private bool InBounds()
+    {
+        if (player.transform.position.x < xLowerBound 
+            || player.transform.position.x > xUpperBound 
+            || player.transform.position.y < yLowerBound
+            || player.transform.position.y > yUpperBound
+            )
+            return false;
+
+        return true;
+    }
+}
---
+++ FirstGamePrototype/Assets/__Scripts/LaserScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    public float movespeed;
+    private Vector3 direction = Vector3.right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * movespeed * Time.deltaTime);
+    }
+}
