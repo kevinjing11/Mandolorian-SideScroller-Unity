b2e15812bb58dbcb39ac3c6bae424348b8de808e
Jack Cochran <64762170+jackscochran@users.noreply.github.com>
2021-02-19 18:11:57 -0700

    Added baby yoda
    Added baby yoda sprite as a prefab with a script to make him float up and down.

---
+++ FirstGamePrototype/Assets/__Scripts/BabyYodaMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyYodaMovement : MonoBehaviour
+{
+    public float amplitude = 0.01f;
+    public float frequency = 1f;
+
+    //Position reference variables
+    Vector2 tempPos = new Vector2();
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make object float up and down using sin function
+        tempPos = transform.position;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
+    }
+}
