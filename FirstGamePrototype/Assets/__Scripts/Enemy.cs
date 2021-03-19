20d747e42f25aaf59d83889c2a44109fcf272fd2
pforn <71098707+pforn@users.noreply.github.com>
2021-03-19 14:30:54 -0400

    added a couple enemy scripts
    pete

---
+++ FirstGamePrototype/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int moveSpeed;
+    int attackDamage;
+    int lifePoints;
+    float attackRadius;
+
+    //movement
+    float followRadius;
+
+        public void setMoveSpeed(int speed)
+    {
+        moveSpeed = speed;
+    }
+
+    public void setAttackDamage(int attdmg)
+    {
+        attackDamage = attdmg;
+    }
+
+    public void setLifePoints(int lp)
+    {
+        lifePoints = lp;
+    }
+
+    public int getMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public int getAttackDamage()
+    {
+        return attackDamage;
+    }
+
+    public int getLifePoints()
+    {
+        return lifePoints;
+    }
+
+        //movement toward a player
+    public void setFollowRadius(float r)
+    {
+        followRadius = r;
+    }
+    //attack radius 
+    public void setAttackRadius(float r)
+    {
+        attackRadius = r;
+    }
+
+        //if player in radius move toward him 
+    public bool checkFollowRadius(float playerPosition, float enemyPosition)
+    {
+        if(Mathf.Abs(playerPosition-enemyPosition) < followRadius)
+        {
+            //player in range
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+        //if player in radius attack him
+    public bool checkAttackRadius(float playerPosition, float enemyPosition)
+    {
+        if (Mathf.Abs(playerPosition - enemyPosition) < attackRadius)
+        {
+            //in range for attack
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FirstGamePrototype/Assets/__Scripts/EnemyMove.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : Enemy
+{
+
+    public int _moveSpeed;
+    public int _attackDamage;
+    public  int _lifePoints;
+    public float _attackRadius;
+
+        //movement
+    public float _followRadius;
+
+    [SerializeField] Transform playerTransform;
+    //[SerializeField]Animator enemyAnim;
+    SpriteRenderer enemySR;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         playerTransform = FindObjectOfType<Hero>().GetComponent<Transform>();
+         //enemyAnim = gameObject.GetComponent<Animator>();
+         enemySR = GetComponent<SpriteRenderer>();
+
+        //set the variables
+        setMoveSpeed(_moveSpeed);
+        setAttackDamage(_attackDamage);
+        setLifePoints(_lifePoints);
+        setAttackRadius(_attackRadius);
+        setFollowRadius(_followRadius);
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (checkFollowRadius(playerTransform.position.x,transform.position.x))
+        {
+            //if player in front of the enemies
+            if (playerTransform.position.x < transform.position.x)
+            {
+
+                if (checkAttackRadius(playerTransform.position.x, transform.position.x))
+                {
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", true);
+                }
+                else
+                {
+                    this.transform.position += new Vector3(-getMoveSpeed() * Time.deltaTime, 0f, 0f);
+                    //for attack animation
+                   // enemyAnim.SetBool("AttackA", false);
+                    //walk
+                   // enemyAnim.SetBool("Walking", true);
+                    enemySR.flipX = true;
+                }
+
+            }
+            //if player is behind enemies
+            else if(playerTransform.position.x > transform.position.x)
+            {
+                if (checkAttackRadius(playerTransform.position.x, transform.position.x))
+                {
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", true);
+                }
+                else
+                {
+                    this.transform.position += new Vector3(getMoveSpeed() * Time.deltaTime, 0f, 0f);
+                    //for attack animation
+                    //enemyAnim.SetBool("AttackA", false);
+                    //walk
+                    //enemyAnim.SetBool("Walking", true);
+                    enemySR.flipX = false;
+                }
+
+
+            }
+        }
+        else
+        {
+            //enemyAnim.SetBool("Walking", false);
+        }
+
+
+    }
+}
