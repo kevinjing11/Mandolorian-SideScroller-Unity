e416816d6abbbff4081e5255604ca59fa8eb4377
kMongru <64448193+kMongru@users.noreply.github.com>
2021-03-23 16:40:43 -0400

    Added Enemy Bounds, Changed Hero Script

--- FirstGamePrototype/Assets/__Scripts/SceneTransitions/EndOfLevel.cs
+++ FirstGamePrototype/Assets/__Scripts/SceneTransitions/EndOfLevel.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndOfLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //maybe add player progression here
+        SceneManager.LoadScene("Cantina");
     }
 }
